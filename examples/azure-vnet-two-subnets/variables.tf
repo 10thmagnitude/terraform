773e6d51e1be7013c06835b302f30af116a31b79
anniehedgpeth <annie.hedgpeth@gmail.com>
2017-04-25 10:54:02 -0500

    deploys locally

--- examples/azure-vnet-two-subnets/variables.tf
+++ examples/azure-vnet-two-subnets/variables.tf
@@ -1,6 +1,6 @@
 variable "resource_group" {
   description = "The name of the resource group in which to create the virtual network."
-  default     = "rg"
+  default     = "rg2"
 }
 
 variable "location" {
